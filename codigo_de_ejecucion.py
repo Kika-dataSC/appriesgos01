d98dfa13a3fb6cae6968983e9ca9488ccf1b6c54
Kika-dataSC <115191404+Kika-dataSC@users.noreply.github.com>
2023-08-03 19:33:36 +0200

    Update codigo_de_ejecucion.py

--- codigo_de_ejecucion.py
+++ codigo_de_ejecucion.py
@@ -41,27 +41,27 @@ def ejecutar_modelos(df):
 
     #6.CARGA PIPES DE EJECUCION
     with open('pipe_ejecucion_pd.pickle', mode='rb') as file:
-       pipe_ejecucion_pd = pickle.load(file)
+    pipe_ejecucion_pd = pickle.load(file)
 
     with open('pipe_ejecucion_ead.pickle', mode='rb') as file:
-       pipe_ejecucion_ead = pickle.load(file)
+    pipe_ejecucion_ead = pickle.load(file)
 
     with open('pipe_ejecucion_lgd.pickle', mode='rb') as file:
-       pipe_ejecucion_lgd = pickle.load(file)
+    pipe_ejecucion_lgd = pickle.load(file)
 
-     #7.EJECUCION
-     scoring_pd = pipe_ejecucion_pd.predict_proba(x_pd)[:, 1]
-     ead = pipe_ejecucion_ead.predict(x_ead)
-     lgd = pipe_ejecucion_lgd.predict(x_lgd)
+    #7.EJECUCION
+    scoring_pd = pipe_ejecucion_pd.predict_proba(x_pd)[:, 1]
+    ead = pipe_ejecucion_ead.predict(x_ead)
+    lgd = pipe_ejecucion_lgd.predict(x_lgd)
 
 
-     #8.RESULTADO
-     principal = x_pd.principal
-     EL = pd.DataFrame({'principal':principal,
+    #8.RESULTADO
+    principal = x_pd.principal
+    EL = pd.DataFrame({'principal':principal,
                    	'pd':scoring_pd,
                    	'ead':ead,
                    	'lgd':lgd                   
                    	})
-     EL['perdida_esperada'] = round(EL.pd * EL.principal * EL.ead * EL.lgd,2)
+    EL['perdida_esperada'] = round(EL.pd * EL.principal * EL.ead * EL.lgd,2)
      
-     return(EL)
+    return(EL)
